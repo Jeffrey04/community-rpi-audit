e8fe43c5d2c970d69d26ba9321eb51d4ff12e791
Jeffrey04 <mycyberpet@yahoo.com>
2020-09-11 18:07:47 +0800

    actually run the script

--- runner/runner.sh
+++ runner/runner.sh
@@ -6,3 +6,8 @@ then
 fi
 
 curl -XPOST -H"Content-type: application/json" -d'{"key": "'${IFTTT_KEY}'", "event": "tg_bot_test"}' ifttt.coolsilon.com/phone_home
+
+for url in $(shuf test-list.txt);
+do
+    ./miniooni -i $url web_connectivity;
+done
